3a61f6ae61b481e654056aba24c2c84b6fc557d4
T0nci <138928016+T0nci@users.noreply.github.com>
2024-03-18 16:36:06 +0100

    Create foundational styling and containers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './assets/css/style.css';
+import addIcon from './assets/img/add.svg';
+import deleteIcon from './assets/img/delete.svg';
+import editIcon from './assets/img/edit.svg';
+import expandIcon from './assets/img/expand.svg';
+import shrinkIcon from './assets/img/shrink.svg';
 
-console.log('Hi!');
