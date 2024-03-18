e68600269aa90c8cc6defb697baf9e0d1c8a22e1
T0nci <138928016+T0nci@users.noreply.github.com>
2024-03-18 20:40:55 +0100

    Add icons for creating projects and closing modals

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './assets/css/style.css';
-import addIcon from './assets/img/add.svg';
 import deleteIcon from './assets/img/delete.svg';
 import editIcon from './assets/img/edit.svg';
 import expandIcon from './assets/img/expand.svg';
 import shrinkIcon from './assets/img/shrink.svg';
+import domModule from './assets/js/dom';
 
